5e10a034276528ecdf09e1b358dbdb0b51e5004a
준써크 <junsuk8960@naver.com>
2022-11-13 02:09:23 +0900

    chap7hw first upload

--- mission/chap7hw/app/src/main/java/com/kimjunseok/chap7hw/MainActivity.kt
+++ mission/chap7hw/app/src/main/java/com/kimjunseok/chap7hw/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.kimjunseok.chap7hw
 
+import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.kimjunseok.chap7hw.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var getResultText: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getResultText = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == RESULT_OK) {
+                binding.etTime.setText(null)
+                val mPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.sound_alarm)
+                mPlayer.start()
+                // SoundPool 사용해보기
+                val mString = result.data?.getStringExtra("timer_minute")
+                val sString = result.data?.getStringExtra("timer_second")
+                Toast.makeText(this, "${mString}:${sString} 이 지났습니다.", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        binding.apply {
+            btnNext.setOnClickListener {
+                if(etTime.text.isEmpty()) {
+                    Toast.makeText(this@MainActivity, "값을 입력하시오",Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    var time = etTime.text.toString().split(":")
+                    if(time.size != 2) {
+                        Toast.makeText(this@MainActivity, "예시처럼 작성하시오",Toast.LENGTH_SHORT).show()
+                        etTime.setText(null)
+                    } else {
+                        val intent = Intent(this@MainActivity, TimerActivity::class.java)
+                        intent.putExtra("timer_minute", time[0])
+                        intent.putExtra("timer_second", time[1])
+                        getResultText.launch(intent)
+                    }
+                }
+            }
+            btnCancel.setOnClickListener {
+                etTime.setText(null)
+            }
+        }
     }
 }
---
+++ mission/chap7hw/app/src/main/java/com/kimjunseok/chap7hw/TimerActivity.kt
@@ -0,0 +1,53 @@
+package com.kimjunseok.chap7hw
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.kimjunseok.chap7hw.databinding.ActivityTimerBinding
+
+class TimerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTimerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTimerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val extras = intent.extras
+        var mString = extras!!["timer_minute"] as String
+        var sString = extras!!["timer_second"] as String
+        val intent = Intent(this@TimerActivity, MainActivity::class.java)
+        intent.putExtra("timer_minute",mString)
+        intent.putExtra("timer_second",sString)
+        var handler = Handler(mainLooper)
+
+        var minute = mString.toInt()
+        var second = sString.toInt()
+
+        binding.apply {
+            timer.setText(String.format("%02d:%02d",minute, second))
+            btnStart.setOnClickListener {
+                Thread() {
+                    while(minute != 0 || second != 0) {
+                        if(second == 0) {
+                            minute--
+                            second = 60
+                        }
+                        else second--
+                        handler.post {
+                            timer.setText(String.format("%02d:%02d",minute, second))
+                        }
+                        Thread.sleep(1000)
+                    }
+                    setResult(RESULT_OK, intent)
+                    if(!isFinishing) finish()
+                }.start()
+            }
+
+            // 정지 버튼 구현하기
+            btnStop.setOnClickListener {
+
+            }
+        }
+
+    }
+}
